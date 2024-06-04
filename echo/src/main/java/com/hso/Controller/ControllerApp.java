347d7cf9253ab76c482bc4d056a18a42bdbf0d0b
WickedJack99 <wickedjack1@proton.me>
2024-06-04 12:30:23 +0200

    Added basic class for controller app.

---
+++ echo/src/main/java/com/hso/Controller/ControllerApp.java
@@ -0,0 +1,13 @@
+package com.hso.Controller;
+
+import com.hso.AppInfo.AppInfo;
+
+/**
+ * @author Aaron Moser
+ */
+
+public class ControllerApp extends Thread {
+    public ControllerApp(AppInfo appInfo) {
+
+    }
+}
