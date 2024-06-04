cb2435b23b10235e489197cdbcd3cf9734f85938
WickedJack99 <wickedjack1@proton.me>
2024-06-04 12:31:24 +0200

    Added log message.

---
+++ echo/src/main/java/com/hso/Messages/LogMessage.java
@@ -0,0 +1,46 @@
+package com.hso.Messages;
+
+import com.hso.Peer.Peer;
+
+/**
+ * @author Aaron Moser
+ */
+
+public final class LogMessage implements Sendable, Receivable {
+
+    private final Peer receiver;
+
+    private final String timestamp;
+    private final Peer start;
+    private final Peer target;
+    private final EMessageType messageType;
+
+    public LogMessage(String timestamp, Peer start, Peer target, EMessageType messageType, Peer receiver) {
+        this.receiver = new Peer(receiver);
+
+        this.timestamp = timestamp;
+        this.start = new Peer(start);
+        this.target = new Peer(target);
+        this.messageType = messageType;
+    }
+
+    @Override
+    public byte[] getMessage() {
+        String request =
+            "{\"messageType\":\"log\"" +
+            "," + 
+            "\"timestamp\":\"" + timestamp + "\"" +
+            "," + 
+            "\"start\":\"" + start.getIPAddress() + ":" + start.getPort() + "\"" +
+            "," + 
+            "\"target\":\"" + target.getIPAddress() + ":" + target.getPort() + "\"" +
+            "," + 
+            "\"receivedMessageType\":\"" + messageType.toString() + "\"}";
+        return request.getBytes();
+    }
+
+    @Override
+    public Peer getReceiver() {
+        return receiver;
+    }
+}
