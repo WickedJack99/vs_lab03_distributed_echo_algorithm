78af6aa9e1115f4b887cdc33314ee053c17cd4c2
WickedJack99 <wickedjack1@proton.me>
2024-06-04 12:31:11 +0200

    Added info message.

---
+++ echo/src/main/java/com/hso/Messages/InfoMessage.java
@@ -0,0 +1,34 @@
+package com.hso.Messages;
+
+import com.hso.Peer.Peer;
+
+/**
+ * @author Aaron Moser
+ */
+
+public final class InfoMessage implements Sendable, Receivable {
+
+    private final Peer receiver;
+
+    private Peer parent = null;
+
+    public InfoMessage(Peer parent, Peer receiver) {
+        this.receiver = new Peer(receiver);
+
+        this.parent = new Peer(parent);
+    }
+
+    @Override
+    public byte[] getMessage() {
+        String request =
+            "{\"messageType\":\"info\"" +
+            "," + 
+            "\"parent\":\"" + parent.getIPAddress() + ":" + parent.getPort() + "\"}";
+        return request.getBytes();
+    }
+
+    @Override
+    public Peer getReceiver() {
+        return receiver;
+    }
+}
